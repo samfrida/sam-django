d1957cbdb7f77148a345496d8cad27757fb37e3b
Samantha Frida <sfrida@gmail.com>
2019-11-27 12:39:33 -0800

    Initial files for django application

---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+import views
+
+# In this example, we've separated out the views.py into a new file
+urlpatterns = [
+    path('', views.about),
+    path('experience', views.experience),
+    path('interest', views.interest),
+    path('education', views.education),
+]
+
+# Boilerplate to include static files
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
---
+++ views.py
@@ -0,0 +1,74 @@
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    # This is similar to ones we have done before. Instead of keeping
+    # the HTML / template in a separate directory, we just reply with
+    # the HTML embedded here.
+    return HttpResponse('''
+        <h1>Welcome to my home page!</h1>
+        <a href="/about-me">About me</a> <br />
+        <a href="/github-api-example">See my GitHub contributions</a> <br />
+    ''')
+
+
+
+
+def about(request):
+    # Django comes with a "shortcut" function called "render", that
+    # lets us read in HTML template files in separate directories to
+    # keep our code better organized.
+    content_html = open ("content/index.html").read()
+    context = {
+        'content': content_html,
+        'title':"About"
+    }
+    return render(request, "base.html", context)
+
+
+def experience(request):
+    # Django comes with a "shortcut" function called "render", that
+    # lets us read in HTML template files in separate directories to
+    # keep our code better organized.
+    content_html = open ("content/Experience.html").read()
+    context = {
+        'content': content_html,
+        'title':"Experience"
+    }
+    return render(request, "templates/base.html", context)
+
+def education(request):
+    # Django comes with a "shortcut" function called "render", that
+    # lets us read in HTML template files in separate directories to
+    # keep our code better organized.
+    content_html = open ("content/Education.html").read()
+    context = {
+        'content': content_html,
+        'title':"Education"
+    }
+    return render(request, "templates/base.html", context)
+
+def interest(request):
+    # Django comes with a "shortcut" function called "render", that
+    # lets us read in HTML template files in separate directories to
+    # keep our code better organized.
+    content_html = open ("content/Interest.html").read()
+    context = {
+        'content': content_html,
+        'title':"Interest"
+    }
+    return render(request, "templates/base.html", context)
+
+
+
+
+def github_api_example(request):
+    # We can also combine Django with APIs
+    response = requests.get('https://api.github.com/users/michaelpb/repos')
+    repos = response.json()
+    context = {
+        'github_repos': repos,
+    }
+    return render(request, 'github.html', context)
+
