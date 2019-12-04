46a8163e66b5f2d9364b1fa2196e8793ed62e610
jamesstevick <jstevick@umich.edu>
2019-12-03 22:12:40 -0800

    Remove github api to test

--- urls.py
+++ urls.py
@@ -5,10 +5,10 @@ import views
 # In this example, we've separated out the views.py into a new file
 urlpatterns = [
     path('', views.about),
-    path('/about/', views.about),
-    path('/experience/', views.experience),
-    path('/interest/', views.interest),
-    path('/education/', views.education),
+    path('about/', views.about),
+    path('experience/', views.experience),
+    path('interest/', views.interest),
+    path('education/', views.education),
 ]
 
 # Boilerplate to include static files
