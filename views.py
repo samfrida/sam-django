538b090a7c0144311e0019e4fdd4375951453edf
jamesstevick <jstevick@umich.edu>
2019-12-03 22:50:23 -0800

    django template

--- views.py
+++ views.py
@@ -17,49 +17,45 @@ def about(request):
     # lets us read in HTML template files in separate directories to
     # keep our code better organized.
     # content_html = open ("content/index.html").read()
-    response = requests.get('https://api.github.com/users/samfrida/repos')
-    repos = response.json()
-    context = {
-        # 'content': content_html,
-        # 'title':"About",
-        # 'github_repos': repos,
-    }
+    # response = requests.get('https://api.github.com/users/samfrida/repos')
+    # repos = response.json()
+    context = {}
     # return render(request, "base.html", context)
-    return render(request, "index.html", context)
+    return render(request, "content/index.html", context)
 
 
 def experience(request):
     # Django comes with a "shortcut" function called "render", that
     # lets us read in HTML template files in separate directories to
     # keep our code better organized.
-    content_html = open ("content/Experience.html").read()
+    # content_html = open ("content/Experience.html").read()
     context = {
-        'content': content_html,
-        'title':"Experience"
+        # 'content': content_html,
+        # 'title':"Experience"
     }
-    return render(request, "base.html", context)
+    return render(request, "content/Experience.html", context)
 
 def education(request):
     # Django comes with a "shortcut" function called "render", that
     # lets us read in HTML template files in separate directories to
     # keep our code better organized.
-    content_html = open ("content/Education.html").read()
+    # content_html = open ("content/Education.html").read()
     context = {
-        'content': content_html,
-        'title':"Education"
+        # 'content': content_html,
+        # 'title':"Education"
     }
-    return render(request, "base.html", context)
+    return render(request, "content/Education.html", context)
 
 def interest(request):
     # Django comes with a "shortcut" function called "render", that
     # lets us read in HTML template files in separate directories to
     # keep our code better organized.
-    content_html = open ("content/Interest.html").read()
+    # content_html = open ("content/Interest.html").read()
     context = {
-        'content': content_html,
-        'title':"Interest"
+        # 'content': content_html,
+        # 'title':"Interest"
     }
-    return render(request, "base.html", context)
+    return render(request, "content/Interest.html", context)
 
 
 def github_api_example(request):
